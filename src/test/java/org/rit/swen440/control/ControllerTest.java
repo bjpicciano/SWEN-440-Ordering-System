620bd069ecfa77b2dc22e445cffc76634ea2ea0a
Brian Picciano <BJPicciano@gmail.com>
2018-12-09 17:56:27 -0500

    created first test

--- src/test/java/org/rit/swen440/control/ControllerTest.java
+++ src/test/java/org/rit/swen440/control/ControllerTest.java
@@ -1,32 +1,44 @@
 package org.rit.swen440.control;
 
 import org.junit.Test;
+import org.rit.swen440.models.Category;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class ControllerTest {
-
+    private Controller controller = new Controller();
     @Test
     public void getTransactionsBetweenDates() {
+
     }
 
     @Test
     public void getTopAndBottomSoldProducts() {
+
     }
 
     @Test
     public void login() {
+
     }
 
     @Test
     public void getAllCategories() {
+        List<Category> categories = controller.getAllCategories();
+
+        if (categories.isEmpty())
+            fail();
     }
 
     @Test
     public void getProductsInCategory() {
+
     }
 
     @Test
     public void createTransaction() {
+
     }
 }
