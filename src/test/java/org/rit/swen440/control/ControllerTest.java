f76ec13d2c7a0a2e7a55a7d1b19c1a1c740595bb
Brian Picciano <BJPicciano@gmail.com>
2018-12-09 17:51:31 -0500

    added skeleton tests

---
+++ src/test/java/org/rit/swen440/control/ControllerTest.java
@@ -0,0 +1,32 @@
+package org.rit.swen440.control;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+    @Test
+    public void getTransactionsBetweenDates() {
+    }
+
+    @Test
+    public void getTopAndBottomSoldProducts() {
+    }
+
+    @Test
+    public void login() {
+    }
+
+    @Test
+    public void getAllCategories() {
+    }
+
+    @Test
+    public void getProductsInCategory() {
+    }
+
+    @Test
+    public void createTransaction() {
+    }
+}
